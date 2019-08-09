826e56b6eae777d649cfa19b806f7ed32e4e352f
Dawid Czopek <d.czopek@emotivo.pl>
2019-08-09 14:16:22 +0200

    master: Modify script for scrolltop

--- js/script.js
+++ js/script.js
@@ -14,21 +14,21 @@
 
     function animateScroll() {
         if(document.scrollingElement.scrollTop > 0){
-            window.scrollBy(0, -5);
-            setTimeout(animateScroll(), 10);
+            // window.scrollTo(0, -5);
+            // setTimeout(animateScroll, 10);
+            window.scroll({"top": 0, behavior: "smooth"})
         }
     }
 
-    button.addEventListener("click", function(){
-        e.stopPropagation();
+    button.addEventListener("click", function(e){
         animateScroll();
+        e.stopPropagation();
     }, false);
 
-    window.addEventListener("scroll", function(e){
+    window.addEventListener("scroll", function(){
         if(document.scrollingElement.scrollTop >= 100) {
             button.classList.remove("hidden");
         } else {
-            console.log('test');
             button.classList.add("hidden");
         }
     }, false);
